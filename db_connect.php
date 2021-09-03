d83c182f3ec78de5ab5166b7816f175715ad737c
markkevinbrin <58113589+markkevinbrin@users.noreply.github.com>
2021-09-03 20:33:31 +0800

    Update db_connect.php
    removed cloud DB username and password

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 
 //$errorlog = 'error_log.txt';
 	
-$connection = mysqli_connect('localhost','root','1Got1Udont!@');
+$connection = mysqli_connect('localhost','','');
 if (!$connection){
 	//file_put_contents($errorlog, mysqli_error($connection) . PHP_EOL, FILE_APPEND);
 	die("Database Connection Failed".mysqli_error($connection));
